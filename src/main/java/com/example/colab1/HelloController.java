7de80d23a9d2c3a5b14bc87e90c39d1bfe7dd31c
20253rd037@utez.edu.mx <20253rd037@utez.edu.mx>
2026-03-30 23:23:32 -0600

    cambios corregidos

--- src/main/java/com/example/colab1/HelloController.java
+++ src/main/java/com/example/colab1/HelloController.java
@@ -1,14 +1,49 @@
 package com.example.colab1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import  javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-}
+    protected TextField txt1;
+    @FXML
+    protected TextField txt2;
+    @FXML
+    protected Label elsee;
+
+    public void abrirVentana (ActionEvent event){
+
+        String usuario = "eloys";
+        String contrasenia = "admin";
+
+        String intentosUsuario;
+        String intentosContrasenioas;
+
+        intentosUsuario = txt1.getText();
+        intentosContrasenioas = txt2.getText();
+
+        if (intentosUsuario.equals(usuario)&&intentosContrasenioas.equals(contrasenia)){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("segundaVista.fxml"));
+            try {
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setTitle("segunda vista");//hola
+                stage.setScene(new Scene(root));
+                stage.show();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }else {
+            elsee.setText("usuario y contrasenia incorrectos");
+        }
+
+    }}
