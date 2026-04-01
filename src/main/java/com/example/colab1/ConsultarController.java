6914b0ea6aebbc5dd1239153cf7cba4009e76e5f
Landeros3165p <20253rd051@utez.edu.mx>
2026-03-31 22:21:31 -0600

    agregue correctamente mi boton saldo

--- src/main/java/com/example/colab1/ConsultarController.java
+++ src/main/java/com/example/colab1/ConsultarController.java
@@ -9,28 +9,24 @@ import javafx.scene.control.Label;
 
 public class ConsultarController {
 
-@FXML
-private TextField Saldo1;
-@FXML
+    @FXML
+    private TextField Saldo1;
+    @FXML
     private Label Muestra;
-@FXML
+    @FXML
     private double Dinero = 0 ;
     @FXML
     private double monto = 0 ;
 
-public void OnSaldo(){
-if(!Saldo1.getText().isEmpty())
-    monto = Double.parseDouble(Saldo1.getText());
-    Dinero += monto;
-
-    Muestra.setText("Saldo actualizado");
-}
-
-
-
-
-
-
+    public void OnSaldo(){
+        Saldo1.setVisible(true);
+        if(!Saldo1.getText().isEmpty())
+            monto = Double.parseDouble(Saldo1.getText());
+        Dinero += monto;
+        Saldo1.clear();
+        Muestra.setText("Saldo actualizado"+monto);
+        Muestra.setVisible(true);
+    }
 
 
 }
