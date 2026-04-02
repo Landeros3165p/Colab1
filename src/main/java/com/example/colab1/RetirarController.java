f1d4e8766fd6beb04fe15c1f8b4cbbfc10b413e4
Evelinenkyang <20253rd059@utez.edu.mx>
2026-04-01 20:20:18 -0600

    añadi los atributos

--- src/main/java/com/example/colab1/RetirarController.java
+++ src/main/java/com/example/colab1/RetirarController.java
@@ -1,6 +1,17 @@
 package com.example.colab1;
 
-public class RetirarController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
+
+public class RetirarController {
+@FXML
+    private TextField montoTxt;
+@FXML
+    private Label textMensaje;
+
+@FXML
 
 }
